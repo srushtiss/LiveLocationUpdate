8d53b1510af034c40f52775b783637677c0fe77c
Srushti Sachdev <43631563+srushtiss@users.noreply.github.com>
2023-08-28 11:45:48 -0500

    Add files via upload

---
+++ DeliveryBoy/src/main/java/com/project/deliveryboy/service/KafkaService.java
@@ -0,0 +1,22 @@
+ package com.project.deliveryboy.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import com.project.deliveryboy.config.AppConstants;
+
+@Service
+public class KafkaService {
+	
+	@Autowired
+	private KafkaTemplate<String,String> kafkaTemplate;
+	
+	public boolean updateLocation(String location) {
+		
+			this.kafkaTemplate.send(AppConstants.LOCATION_TOPIC_NAME,location);
+		
+		
+		return true;
+	}
+}
---
+++ DeliveryBoy/src/test/java/com/project/deliveryboy/DeliveryBoyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.deliveryboy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryBoyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
